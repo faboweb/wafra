a00b0a85987290e56503b1b1d3cccddab5889829
Fabian Weber <frznhope@gmail.com>
2025-02-02 17:23:24 +0530

    merge depsit address gen

--- apps/server/src/api.ts
+++ apps/server/src/api.ts
@@ -11,35 +11,6 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post("/deposit/new", async (req: any, res: any) => {
-  try {
-    const headers = req.headers;
-    const authorization = headers.authorization;
-    if (authorization !== process.env.AUTHORIZATION) {
-      return res.status(401).send({ message: "Unauthorized" });
-    }
-
-    const depositData = req.body;
-    const address = depositData.address;
-    if (!address || !ethers.isAddress(address)) {
-      return res.status(400).send({ message: "Address is required" });
-    }
-
-    const wallet = ethers.Wallet.createRandom();
-    await prisma.wallet.create({
-      data: {
-        address,
-        depositAddress: wallet.address,
-        privateKey: wallet.privateKey,
-      },
-    });
-
-    res.status(200).send({ depositAddress: wallet.address });
-  } catch (err) {
-    res.status(500).send({ message: "Internal Server Error" });
-  }
-});
-
 app.get("/deposit/address", async (req: any, res: any) => {
   try {
     const headers = req.headers;
@@ -53,17 +24,25 @@ app.get("/deposit/address", async (req: any, res: any) => {
     if (!address || !ethers.isAddress(address)) {
       return res.status(400).send({ message: "Address is required" });
     }
-    const wallet = await prisma.wallet.findFirst({
+
+    let depositWallet = await prisma.wallet.findFirst({
       where: {
         address,
       },
     });
 
-    if (!wallet) {
-      res.status(500).send({ error: "Wallet not initialized" });
-      return;
+    if (!depositWallet) {
+      depositWallet = ethers.Wallet.createRandom();
+      await prisma.wallet.create({
+        data: {
+          address,
+          depositAddress: depositWallet.address,
+          privateKey: depositWallet.privateKey,
+        },
+      });
     }
-    res.status(200).send({ depositAddress: wallet.address });
+
+    res.status(200).send({ depositAddress: depositWallet.address });
   } catch (err) {
     res.status(500).send({ message: "Internal Server Error" });
   }
